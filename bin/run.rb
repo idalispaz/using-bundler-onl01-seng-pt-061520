7b0eeb64f0a4573601db848057ba121166d778a0
idalispaz <idalispaz@gmail.com>
2020-07-26 13:31:16 -0700

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,7 @@
+# this file will start our application. It will also require the environment file that was created earlier to provide our app with access to our gems
+
+require_relative '../config/environment' #takes a relative path that is relative to the file in which the required statement is called 
+
 Hashie
 Sinatra
 Octokit
---
+++ config/environment.rb
@@ -0,0 +1,3 @@
+# this file is where we'll be loading all of our app's dependencies, from gems to database connections.
+require 'bundler/setup'
+Bundler.require(:default, :development)
